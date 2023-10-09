dbdc18dc001b328215f26ab41c625e4bb1a8bac6
yousseffahmed <yoyo_ah360@hotmail.com>
2023-10-09 20:46:49 +0300

    debugging some issues

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); // Import the body-parser middleware
 require('dotenv').config();
 
 const uri = process.env.MONGODB_URI;
@@ -17,6 +18,13 @@ db.once('open', () => {
   console.log('Connected to MongoDB Atlas');
 });
 
+const medicineController = require('./controller/medicineController');
+
+app.use(express.json());
+
+app.get('/medicines', medicineController.getMedicines);
+
+app.post('/createMedicines', medicineController.createMedicine);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ src/controller/medicineController.js
@@ -0,0 +1,46 @@
+const Medicine = require('../model/medicine');
+
+exports.getMedicines = async (req, res) => {
+  try {
+    const medicines = await Medicine.find();
+    res.json(medicines);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.createMedicine = async (req, res) => {
+  try {
+    const {
+      name,
+      dosage,
+      description,
+      medUse,
+      detail,
+      quantity,
+      sales,
+    } = req.body;
+
+    const newMedicine = new Medicine({
+      name,
+      dosage,
+      description,
+      medUse,
+      detail,
+      quantity,
+      sales,
+    });
+
+    await newMedicine.save();
+
+    res.status(201).json({
+      message: 'Medicine added successfully',
+      medicine: newMedicine,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Other routes for CRUD operations...
+
